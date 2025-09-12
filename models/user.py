eaab5a5b64f732046b2011b9c6f4c7fd3208dc4f
Keaton Schirmer <keasch1589@gmail.com>
2025-09-12 13:52:57 -0500

    tablename fix

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@ from database.db import db
 
 class User(db.Model):
 
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     is_admin = db.Column(db.Boolean, default=False)
     role = db.Column(db.String(10))
