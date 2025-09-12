8f460f199c8e52d5c70d8fe10169033623e54921
Keaton Schirmer <keasch1589@gmail.com>
2025-09-12 12:37:50 -0500

    DB fix

--- routes/contacts.py
+++ routes/contacts.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, jsonify, request
 from models.message import ClientContact
 from models.job import JobRequest
-from models import db
+from database.db import db
 
 clients_bp = Blueprint('contacts', __name__)
 
